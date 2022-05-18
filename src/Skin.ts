60ac1e358783bc36c96f82bca7fbd63b12d30946
cclarice <179danil@gmail.com>
2022-05-18 14:49:36 +0300

    update

--- src/Skin.ts
+++ src/Skin.ts
@@ -1,8 +1,19 @@
 import Jimp from 'jimp'
-import themes, { Theme } from './themes'
+import { Theme } from './themes'
 import md5 from 'md5'
 import arrayFromHash from './arrayFromHash'
 import getThemeFromHash from './Theme'
+
+let shadows = new Jimp(64, 64, (err) => {
+  if (err) {
+    throw err
+  }
+})
+
+Jimp.read('./src/assets/shadows.png')
+  .then((image) => {
+    shadows = image
+  })
 
 export default class Skin {
   username: string
@@ -18,14 +29,16 @@ export default class Skin {
   }
 
   createImageFromHash (fHash = this.hash): Jimp {
-    const image = this.image = new Jimp (64, 64, (err) => {
-      if (err) throw err
+    const image = this.image = new Jimp(64, 64, (err) => {
+      if (err) {
+        throw err
+      }
     })
     const colors = this.theme.colors
     let hash = fHash
     const getHash = (): string => {
-      const newHash = hash = md5(hash)
-      return newHash
+      hash = md5(hash)
+      return hash
     }
 
     // Head
@@ -52,11 +65,12 @@ export default class Skin {
         image.setPixelColor(colors[v], 7 - i % 4 + 16, Math.floor(i / 4))
       })
     // - - Bottom Shadow
-    image.composite(image.clone().crop(18, 2, 4, 4).brightness(-0.75), 18, 2)
 
     // - Sides
-    const faceSide = new Jimp (8, 8, (err) => {
-      if (err) throw err
+    const faceSide = new Jimp(8, 8, (err) => {
+      if (err) {
+        throw err
+      }
     })
     arrayFromHash(getHash())
       .forEach((v, i) => {
@@ -73,25 +87,55 @@ export default class Skin {
         image.setPixelColor(colors[v], i % 4 + 20, Math.floor(i / 4) + 16)
         image.setPixelColor(colors[v], 7 - i % 4 + 20, Math.floor(i / 4) + 16)
       })
-    const bodyShadow = image.clone().crop(20, 16, 8, 4)
+    const bodyShadow = image.clone()
+      .crop(20, 16, 8, 4)
     image.composite(bodyShadow, 28, 16)
-    image.composite(bodyShadow.crop(1, 1, 6, 2).brightness(-0.75), 29, 17)
-    image.composite(bodyShadow.crop(1, 0, 4, 2), 22, 17)
     // - Back
     arrayFromHash(getHash(), 48)
       .forEach((v, i) => {
-        image.setPixelColor(colors[v], i % 4 + 28, Math.floor(i / 4) + 20)
-        image.setPixelColor(colors[v], 7 - i % 4 + 28, Math.floor(i / 4) + 20)
+        image.setPixelColor(colors[v], i % 4 + 32, Math.floor(i / 4) + 20)
+        image.setPixelColor(colors[v], 7 - i % 4 + 32, Math.floor(i / 4) + 20)
       })
     // - Sides
     arrayFromHash(getHash(), 48)
       .forEach((v, i) => {
         image.setPixelColor(colors[v], i % 4 + 16, Math.floor(i / 4) + 20)
-        image.setPixelColor(colors[v], 7 - i % 4 + 32, Math.floor(i / 4) + 20)
+        image.setPixelColor(colors[v], 7 - i % 4 + 24, Math.floor(i / 4) + 20)
       })
-    const sideShadow = image.clone().crop(17, 21, 2, 2).brightness(-0.75)
-    image.composite(sideShadow, 17, 21)
-    image.composite(sideShadow.flip(false, true), 37, 21)
+
+    // Legs
+    arrayFromHash(getHash(), 64)
+      .forEach((v, i) => {
+        image.setPixelColor(colors[v], i % 4 + 4, Math.floor(i / 4) + 16)
+      })
+    arrayFromHash(getHash(), 64)
+      .forEach((v, i) => {
+        image.setPixelColor(colors[v], 7 - i % 4 + 4, Math.floor(i / 4) + 16)
+      })
+    arrayFromHash(getHash(), 48)
+      .forEach((v, i) => {
+        image.setPixelColor(colors[v], i % 4, Math.floor(i / 4) + 20)
+        image.setPixelColor(colors[v], 7 - i % 4 + 8, Math.floor(i / 4) + 20)
+      })
+    image.composite(image.clone()
+      .crop(0, 16, 16, 16), 16, 48)
+    // Arms
+    arrayFromHash(getHash(), 64)
+      .forEach((v, i) => {
+        image.setPixelColor(colors[v], i % 4 + 44, Math.floor(i / 4) + 16)
+      })
+    arrayFromHash(getHash(), 64)
+      .forEach((v, i) => {
+        image.setPixelColor(colors[v], 7 - i % 4 + 44, Math.floor(i / 4) + 16)
+      })
+    arrayFromHash(getHash(), 48)
+      .forEach((v, i) => {
+        image.setPixelColor(colors[v], i % 4 + 40, Math.floor(i / 4) + 20)
+        image.setPixelColor(colors[v], 7 - i % 4 + 48, Math.floor(i / 4) + 20)
+      })
+    image.composite(image.clone()
+      .crop(40, 16, 16, 16), 32, 48)
+    image.composite(shadows, 0, 0)
 
     return image
   }
@@ -102,11 +146,15 @@ export default class Skin {
 
       image.resize(8 * scale, 8 * scale, Jimp.RESIZE_NEAREST_NEIGHBOR)
       image.write(`${dir}/${this.username}.png`, (err) => {
-        if (err) throw err
+        if (err) {
+          throw err
+        }
       })
     } else {
       this.image.write(`${dir}/${this.username}.png`, (err) => {
-        if (err) throw err
+        if (err) {
+          throw err
+        }
       })
     }
   }
